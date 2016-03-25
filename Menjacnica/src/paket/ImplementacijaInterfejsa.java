01afba0b88bb001753a9f28de35b4570a21fd202
oliverakordic <oljaitacka@gmail.com>
2016-03-25 10:40:15 +0100

    Napisano je telo metoda klase ImplementacijaInterfejsa.

--- Menjacnica/src/paket/ImplementacijaInterfejsa.java
+++ Menjacnica/src/paket/ImplementacijaInterfejsa.java
@@ -4,9 +4,19 @@ import java.util.GregorianCalendar;
 
 
 public class ImplementacijaInterfejsa {
-	public void dodajKursNaDan(GregorianCalendar dan, double kurs){}
-	public void brisiKursNaDan(GregorianCalendar dan){}
+	Kurs k = new Kurs();
+	
+	public void dodajKursNaDan(GregorianCalendar dan, double kurs){
+		k.setDatum(dan);
+		k.setProdajniKurs(kurs);
+	}
+	public void brisiKursNaDan(GregorianCalendar dan){
+		if(k.getDatum().equals(dan))
+			k.setProdajniKurs(0);
+	}
 	public double vratiKursNaDan(GregorianCalendar dan){
+		if(k.getDatum().equals(dan))
+			return k.getProdajniKurs();
 		return 0;
 	}
 
