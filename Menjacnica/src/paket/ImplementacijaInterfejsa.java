7037de6c7d9dcc9f87547a279b82c3f156c81796
oliverakordic <oljaitacka@gmail.com>
2016-03-25 10:31:21 +0100

    Napravljena je klasa koja implementira sve metode interfejsa MenacnicaInterfejs. Za sada su tela metoda prazna.

---
+++ Menjacnica/src/paket/ImplementacijaInterfejsa.java
@@ -0,0 +1,13 @@
+package paket;
+
+import java.util.GregorianCalendar;
+
+
+public class ImplementacijaInterfejsa {
+	public void dodajKursNaDan(GregorianCalendar dan, double kurs){}
+	public void brisiKursNaDan(GregorianCalendar dan){}
+	public double vratiKursNaDan(GregorianCalendar dan){
+		return 0;
+	}
+
+}
