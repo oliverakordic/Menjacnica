eeacf06cca9b58ca49a355e6dcf6695437ceea61
oliverakordic <oljaitacka@gmail.com>
2016-03-25 09:35:44 +0100

    Dodate su metode toString(), equals() i hashCode() u obe klase.

--- Menjacnica/src/paket/Valute.java
+++ Menjacnica/src/paket/Valute.java
@@ -16,6 +16,41 @@ public class Valute {
 	public void setSkraceniNaziv(String skraceniNaziv) {
 		this.skraceniNaziv = skraceniNaziv;
 	}
+	@Override
+	public String toString() {
+		return "Valute [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result
+				+ ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valute other = (Valute) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
 	
 	
 	
