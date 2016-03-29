5f79e23875faa22f4bed91fc9427804b02ac8841
oliverakordic <oljaitacka@gmail.com>
2016-03-29 23:58:45 +0200

    Prepravljena klasa ImplementacijaInterfejsa na master grani.

--- Menjacnica/src/paket/ImplementacijaInterfejsa.java
+++ Menjacnica/src/paket/ImplementacijaInterfejsa.java
@@ -3,28 +3,19 @@ package paket;
 import interfejs.MenjacnicaInterfejs1;
 
 import java.util.GregorianCalendar;
-//import paket.intefrejs.MenjacnicaInterfejs;
 
 
 public class ImplementacijaInterfejsa implements MenjacnicaInterfejs1{
 	public void dodajKursNaDan(GregorianCalendar dan, double kurs){
 		Kurs k = new Kurs();
-		if(dan==null)
-			return;
+
 		k.setDatum(dan);
 		k.setProdajniKurs(kurs);
 	}
 	public void brisiKursNaDan(GregorianCalendar dan){
-		Kurs k = new Kurs();
-		if(dan.equals(null))
-			return;
-		k.setProdajniKurs(0);
-		
+
 	}
 	public double vratiKursNaDan(GregorianCalendar dan){
-		Kurs k = new Kurs();
-		if(dan.equals(null))
-			return 0;
-		return k.getProdajniKurs();
+		return 0;
 	}
 }
